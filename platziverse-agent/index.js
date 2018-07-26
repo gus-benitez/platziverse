88ffda1ca95b12aa16e303a6d7129ceb9c135c28
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-07-26 13:18:41 -0300

    Definition of connection and disconnection of agent. - A timer is included for continuous execution.

--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -3,8 +3,31 @@
 const EventEmitter = require('events')
 
 class PlatziverseAgent extends EventEmitter {
-  constructor () {
+  constructor (opts) {
     super()
+
+    this._options = opts
+    this._started = false
+    this._timer = null
+  }
+
+  connect () {
+    if (!this._started) {
+      this._started = true
+      const opts = this._options
+      this._timer = setInterval(() => {
+        this.emit('agent/message', 'this is a message')
+      }, opts.interval)
+      this.emit('connected')
+    }
+  }
+
+  disconnect () {
+    if (this._started) {
+      this._started = false
+      clearInterval(this._timer)
+      this.emit('disconnected')
+    }
   }
 }
 
