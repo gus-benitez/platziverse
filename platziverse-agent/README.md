7d829e3c3cb6c449cb430ede2fef513d5826504b
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-07-30 21:52:27 -0300

    Implementation of custom metrics.

--- platziverse-agent/README.md
+++ platziverse-agent/README.md
@@ -5,21 +5,43 @@
 ``` js
 const PlatziverseAgent = require('platziverse-agent')
 
-const agent = new Platziverse({
+const agent = new PlatziverseAgent({
+  name: 'myapp',
+  username: 'admin',
   interval: 2000
 })
 
-// Only for this agent
+agent.addMetric('rss', function getRss () {
+  return process.memoryUsage().rss
+})
+
+agent.addMetric('promiseMetric', function getRandomPromise () {
+  return Promise.resolve(Math.random())
+})
+
+agent.addMetric('callbackMetric', function getRandomCallback (callback) {
+  setTimeout(() => {
+    callback(null, Math.random())
+  }, 1000)
+})
+
 agent.connect()
-agent.disconnect()
-agent.message()
+
+// Only for this agent
+agent.on('connected', handler)
+agent.on('disconnected', handler)
+agent.on('message', handler)
 
 // For other agents
-agent.on('agent/connected')
-agent.on('agent/disconnected')
+agent.on('agent/connected', handler)
+agent.on('agent/disconnected', handler)
 agent.on('agent/message', payload => {
   console.log(payload)
 })
 
-setTimeout(() => agent.disconnect(), 20000)
+function handler (payload) {
+  console.log(payload)
+}
+
+setTimeout(() => agent.disconnect(), 10000)
 ```
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const debug = require('debug')('platziverse:agent')
+const os = require('os')
+const util = require('util')
 const defaults = require('defaults')
 const mqtt = require('mqtt')
 const uuid = require('uuid')
@@ -23,31 +25,71 @@ class PlatziverseAgent extends EventEmitter {
     this._options = defaults(opts, options) // Seteamos las opciones definidas.
     this._started = false
     this._timer = null
-    this._client = null // Referencia del cliente.
+    this._clientMqtt = null // Referencia del cliente mqtt.
     this._agentId = null // Referencia al agentId.
+    this._metrics = new Map() // Almacenamos el nombre de la metrica y la funcion con la que obtenemos el valor
+  }
+
+  addMetric (type, fn) {
+    this._metrics.set(type, fn)
+  }
+  removeMetric (type) {
+    this._metrics.delete(type)
   }
 
   connect () {
     if (!this._started) {
       const opts = this._options
       this._started = true
-      this._client = mqtt.connect(opts.mqtt.host) // Nos conectamos con el cliente, pasando el host.
+      this._clientMqtt = mqtt.connect(opts.mqtt.host) // Nos conectamos con el cliente mqtt, pasando el host.
 
       // Nos suscribimos a los mensajes que queremos escuchar.
-      this._client.subscribe('agent/message')
-      this._client.subscribe('agent/connected')
-      this._client.subscribe('agent/disconnected')
+      this._clientMqtt.subscribe('agent/connected')
+      this._clientMqtt.subscribe('agent/disconnected')
+      this._clientMqtt.subscribe('agent/message')
 
-      this._client.on('connect', () => {
+      // El cliente mqtt se conecte exitosamente al servidor Mqtt.
+      this._clientMqtt.on('connect', () => {
         this._agentId = uuid.v4() // Generamos el uuid
         this.emit('connected', this._agentId) // Enviamos el agente
 
-        this._timer = setInterval(() => {
-          this.emit('agent/message', 'this is a message')
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) { // Solo hacemos la transmisión si tengo metricas
+            let message = { // mensaje a transmitir
+              agent: {
+                uuid: this._agentId,
+                username: opts.username,
+                name: opts.name,
+                hostname: os.hostname() || 'localhost', // 'os' es el modulo de sistema operativo de node
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
+
+            // Agregamos las metricas, en función de lo definido en _metrics.
+            for (let [metric, fn] of this._metrics) {
+              if (fn.length === 1) { // Si esta fn tiene un argumento es callback
+                // promisify, transforma una función callback a una promesa. Definida en el modulo util de node.js
+                fn = util.promisify(fn)
+              }
+
+              message.metrics.push({
+                type: metric,
+                value: await Promise.resolve(fn())
+              })
+            }
+
+            debug('Sending', message)
+
+            this._clientMqtt.publish('agent/message', JSON.stringify(message))
+            this.emit('message', message) // Emitimos el mensaje internamente.
+          }
         }, opts.interval)
       })
 
-      this._client.on('message', (topic, payload) => {
+      // Sección cuando el cliente reciba un mensaje.
+      this._clientMqtt.on('message', (topic, payload) => {
         payload = parsePayload(payload)
 
         let broadcast = false
@@ -59,12 +101,12 @@ class PlatziverseAgent extends EventEmitter {
             break
         }
         if (broadcast) {
-          this.emit(topic, payload)
+          this.emit(topic, payload) // Mensaje recibido de otro agente, lo emitimos internamente.
         }
       })
 
       // Con eventEmitter hay que manejar el error.
-      this._client.on('error', () => this.disconnect())
+      this._clientMqtt.on('error', () => this.disconnect())
     }
   }
 
@@ -72,7 +114,8 @@ class PlatziverseAgent extends EventEmitter {
     if (this._started) {
       this._started = false
       clearInterval(this._timer)
-      this.emit('disconnected')
+      this.emit('disconnected', this._agentId) // Emitimos la desconexión del agente.
+      this._clientMqtt.end() // Nos desconectamos del cliente mqtt
     }
   }
 }
