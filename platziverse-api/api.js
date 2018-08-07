181584a95208f9cf16ed86435acae9edb190dc58
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-08-07 20:49:47 -0300

    Integration with the database module.

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -3,8 +3,31 @@
 const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
 const {AgentNotFoundError, MetricsNotFoundError, NotAuthorizedError, NotAuthenticatedError} = require('./custom-error')
+const db = require('platziverse-db')
+const getConfig = require('../platziverse-db/lib/config')
 
 const api = express.Router() // Creamos una instancia de la Clase Router de Express
+
+let services, Agent, Metric
+
+// Usamos un middleware de intersección. Se ejecuta cada vez que se haga una petición.
+api.use('*', async (req, res, next) => { // '*' significa para todas las rutas.
+  // Este middleware se usa para obtener la conexión a la Base de Datos, solo una única vez
+  if (!services) {
+    debug('Connecting to database')
+    try {
+      const config = getConfig({
+        logging: s => debug(s)
+      })
+      services = await db(config)
+    } catch (err) {
+      return next(err) // El error le pasamos al manejador de errores de Express
+    }
+    Agent = services.Agent
+    Metric = services.Metric
+  }
+  next() // Indica que tiene que se continúa la ejecución.
+})
 
 // Definimos las rutas
 // Retorna los agentes conectados en nuestro servidor
