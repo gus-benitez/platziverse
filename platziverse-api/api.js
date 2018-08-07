351ecf2ab371a79ef6667b621568ea3080ba87ff
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-08-06 23:04:10 -0300

    -api_Definición de rutas del API.

---
+++ platziverse-api/api.js
@@ -0,0 +1,33 @@
+'use-strict'
+
+const debug = require('debug')('platziverse:api:routes')
+const express = require('express')
+
+const api = express.Router() // Creamos una instancia de la Clase Router de Express
+
+// Definimos las rutas
+// Retorna los agentes conectados en nuestro servidor
+api.get('/agents', (req, res) => {
+  debug('A request has come to /agents')
+  res.send({}) // Envíamos como respuesta un objeto vacio
+})
+
+// Retorna un agente en particular
+api.get('/agent/:uuid', (req, res) => {
+  const { uuid } = req.params
+  res.send({uuid}) // Respondemos con el uuid que obtuvimos
+})
+
+// Retorna que metricas tiene reportadas un agente específico
+api.get('/metrics/:uuid', (req, res) => {
+  const { uuid } = req.params
+  res.send({uuid}) // Respondemos con el uuid que obtuvimos
+})
+
+// De un agente específico, traigame todas las métricas con un tipo en particular
+api.get('/metrics/:uuid/:type', (req, res) => {
+  const { uuid, type } = req.params
+  res.send({uuid, type}) // Respondemos con el uuid y type que obtuvimos
+})
+
+module.exports = api
--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -4,9 +4,13 @@ const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
 
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express() // Creamos una aplicación de express
 const server = http.createServer(app) // Creamos una instancia del servidor http, pasando la aplicación de express app.
+
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} Server listening on port ${port}`)
