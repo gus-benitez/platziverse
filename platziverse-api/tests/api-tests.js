c84ab877d42070c6b17fbff8eca2218e36a0e7ad
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-08-13 22:27:19 -0300

    Pruebas de Integración con Ava, Supertest y Sinon.

--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -2,8 +2,40 @@
 
 const test = require('ava')
 const request = require('supertest')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
-const server = require('../server')
+const agentFixtures = require('./fixtures/agent')
+
+let sandbox = null // sandbox es un ambiente específico de Sinon
+let server = null
+let dbStub = null // Stub del objeto de base de datos
+let AgentStub = {}
+let MetricStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.createSandbox() // Creamos el sandbox (Ambiente específico de Sinon).
+
+  // Stub de base de datos.
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    Agent: AgentStub,
+    Metric: MetricStub
+  }))
+  // Stub de base de datos.
+  AgentStub.findConnected = sandbox.stub()
+  AgentStub.findConnected.returns(Promise.resolve(agentFixtures.connected))
+
+  const api = proxyquire('../api', {
+    'platziverse-db': dbStub // Cada vez que se requiera 'platziverse-db', se debe retornar dbStub
+  })
+  server = proxyquire('../server', {
+    './api': api // Cuando se requiera 'api', se debe retornar el api que contiene dbStub
+  })
+})
+
+// recreamos el sandbox, cada vez que ejecutamos el test.
+test.afterEach(() => sandbox && sandbox.restore())
 
 // 'cb' significa callback, se lo usa porque supertest usa callback.
 // Si fuera async await, no se usa 'cb'
@@ -16,8 +48,18 @@ test.serial.cb('/api/agents', t => {
     .expect('Content-Type', /json/) // Espero que el tipo de contenido sea JSON
     .end((err, res) => { // Esta sección es cuando termine, hago mas validaciones
       t.falsy(err, 'Should not return an error') // No debe haber un error, el error debe ser falso
-      let body = res.body // Obtenemos el body de la respuesta
-      t.deepEqual(body, {}, 'Response body should be the expected') // El body es = a un objeto vacío
+      let body = JSON.stringify(res.body) // Obtenemos el body de la respuesta y lo llevamos a String
+      let expected = JSON.stringify(agentFixtures.connected)
+      t.deepEqual(body, expected, 'Response body should be the expected')
       t.end() // Indica que la prueba terminó. Esto solo se usa para 'cb' callback
     })
 })
+
+test.serial.todo('api/agent/:uuid')
+test.serial.todo('api/agent/:uuid - not found')
+
+test.serial.todo('api/metrics/:uuid')
+test.serial.todo('api/metrics/:uuid - not found')
+
+test.serial.todo('api/metrics/:uuid/:type')
+test.serial.todo('api/metrics/:uuid/:type - not found')
