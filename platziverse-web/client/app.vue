d215c0d1ada0b8527de9b0b64c41be6142ed85cd
Gustavo Benitez <ing.benitez.gustavo@gmail.com>
2018-08-29 17:51:16 -0300

    Introducimos Vue.js en Node

---
+++ platziverse-web/client/app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <H2>TESTE</H2>
+<!--
+    <agent
+      v-for="agent in agents"
+      :uuid="agent.uuid"
+      :key="agent.uuid">
+    </agent>
+-->
+    <p v-if="error">{{error}}</p>
+  </div>
+</template>
+
+<script>
+  const io = require('socket.io-client')
+  const socket = io()
+  module.exports = {
+    data () {
+      return {
+        agents: [],
+        error: null,
+        socket
+      }
+    },
+    mounted () {
+      this.initialize()
+    },
+    methods: {
+      initialize () {
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    font-family: Arial;
+    background: #f8f8f8;
+    margin: 0;
+  }
+</style>
